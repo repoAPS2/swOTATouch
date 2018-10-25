171e163532197d04a7d2c1b949177456b2f2c500
HannaSokolsky <hannasokolsky@gmail.com>
2018-10-24 17:17:22 -0700

    First commit

---
+++ src/main/java/com/versonix/App.java
@@ -0,0 +1,13 @@
+package com.versonix;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( " **   Hello World!    Running java class as java app *** " );
+    }
+}
---
+++ src/test/java/com/versonix/SimpleTestNGTest.java
@@ -0,0 +1,17 @@
+package com.versonix;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by anna on 10/24/18.
+ */
+public class SimpleTestNGTest {
+
+     @Test
+     public void firstTest (){
+
+          System.out.println(" ...  Simple test runs successfully..");
+
+     }
+
+}
